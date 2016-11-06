33ed9e172717ef47955a3fb20a069203fd829df2
adaptedbee <adaptedbee@yandex.ru>
2016-11-06 03:33:49 +0300

    Задание 4.2

---
+++ 4.2/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({"extended": true}));
+
+const port = process.env.PORT || 1337;
+
+app.get('/', (req, res) => {
+  res.status(200).send("Hello Express.js");
+});
+
+app.get('/hello', (req, res) => {
+  res.status(200).send("Hello stranger !");
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.status(200).send(`Hello, ${req.params.name} !`);
+});
+
+app.all('/sub/*/*', (req, res) => {
+  res.status(200).send(`You requested URI: ${req.originalUrl}`);
+});
+
+const keyCheck = (req, res, next) => {
+  if(req.get('key') === undefined) {
+    next(new Error('401'));
+  }
+  next();
+};
+
+app.post('/post', keyCheck, (req, res) => {
+  if (req.body !== undefined){
+    res.json(JSON.stringify(req.body));
+  } else {
+    res.status(404).send("Not Found");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening port ${port}...`);
+});
