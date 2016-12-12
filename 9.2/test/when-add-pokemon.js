4f30c44bd632728489bbf605694c413d413cae98
adaptedbee <adaptedbee@yandex.ru>
2016-12-13 00:18:31 +0300

    Тест валидации формы

--- 9.2/test/when-add-pokemon.js
+++ 9.2/test/when-add-pokemon.js
@@ -1,6 +1,6 @@
 describe('When add new pokemon', function() {
-    describe('PokemonApp should', function() {
-        it('return OK message', function() {
+    describe('and name field is not empty', function() {
+        it('PokemonApp should return confirm message', function() {
             browser.get('http://localhost:8000/app/#!/create');
 
             element(by.model('newPokemon.name')).sendKeys('TestName');
@@ -11,6 +11,18 @@ describe('When add new pokemon', function() {
 
             var confirmMessage = element(by.id('confirmMessage')).getText();
             expect(confirmMessage).toContain("Привет");
+        });
+    });
+
+    describe('and name field is empty', function() {
+        it('form should be invalid', function() {
+            browser.get('http://localhost:8000/app/#!/create');
+
+            element(by.model('newPokemon.weight')).sendKeys(100);
+            element(by.model('newPokemon.height')).sendKeys(55);
+
+            var formClass = element(by.name('pokemonForm')).getAttribute('class');
+            expect(formClass).toContain("ng-invalid");
         });
     });
 });
