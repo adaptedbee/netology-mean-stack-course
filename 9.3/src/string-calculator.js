4b5fdfa5fb5611f853ceadc6bbf41169c32f8a17
adaptedbee <adaptedbee@yandex.ru>
2016-12-19 20:07:47 +0300

    Kata task 1.1

---
+++ 9.3/src/string-calculator.js
@@ -0,0 +1,19 @@
+var Calculator = function() {
+
+    var self = this;
+
+    self.add = function(input) {
+        if (!input) {
+            return 0;
+        }
+
+        if (input.indexOf(',') === -1){
+            return parseInt(input);
+        } else {
+            numbers = input.split(/[\n,]/);
+            return parseInt(numbers[0]) + parseInt(numbers[1]);
+        }
+    };
+}
+
+module.exports = Calculator;
---
+++ 9.3/test/string-calculalator.test.js
@@ -0,0 +1,29 @@
+var assert = require('chai').assert;
+var Calculator = require('../src/string-calculator');
+
+describe('string calculator should', function() {
+    it('return 0 for an empty string', function() {
+        var calculator = new Calculator();
+
+        var result = calculator.add("");
+
+        assert.equal(result, 0);
+    });
+
+    it('return single number if input is that number', function() {
+        var calculator = new Calculator();
+
+        var sum = calculator.add("1");
+
+        assert.equal(sum, 1);
+    });
+
+    it('return sum of any two comma separated values', function() {
+        var calculator = new Calculator();
+
+        var sum = calculator.add("1,2")
+
+        assert.equal(sum, 1 + 2);
+    });
+
+});
