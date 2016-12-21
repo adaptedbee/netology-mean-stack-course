c8d092c96f6375ef745c2220c754f1e64c31e99c
adaptedbee <adaptedbee@yandex.ru>
2016-12-21 22:00:01 +0300

    Returns sum of unknown amount of numbers separated by custom delimeter if first line is not provided

--- 9.3/src/string-calculator.js
+++ 9.3/src/string-calculator.js
@@ -3,32 +3,45 @@ var Calculator = function() {
     var self = this;
 
     self.add = function(input) {
+        var numbers = [];
+        var customDelimeter;
+        var reg;
+        var hasDelimeters = false;
+
         if (!input) {
             return 0;
         }
 
-        if ((input.indexOf(',') === -1) && (input.indexOf('\n') === -1)){
-            return parseInt(input);
+        if (input.indexOf('//') === 0) {
+            var firstNewLinePosition = input.indexOf('\n');
+
+            customDelimeter = input.substring(2, firstNewLinePosition);
+            reg = new RegExp(customDelimeter);
+            numbers = input.split(reg);
+
+            numbers[0] = 0;
+            numbers[1] = numbers[1].substr(1,numbers[1].length);
+        } else if ((input.indexOf(',') !== -1) || (input.indexOf('\n') !== -1)) {
+            numbers = input.split(/[\n,]/);
         } else {
-            if (input.indexOf('//') === 0) {
-                var firstNewLinePosition = input.indexOf('\n');
-
-                var customDelimeter = input.substring(2, firstNewLinePosition);
-                var reg = new RegExp(customDelimeter);
-
-                numbers = input.split(reg);
-
-                numbers[0] = 0;
-                numbers[1] = numbers[1].substr(1,numbers[1].length);
-            } else {
-                numbers = input.split(/[\n,]/);
+            for (var i=0; i<input.length; i++){
+                if (isNaN(input[i])) {
+                    hasDelimeters = true;
+                    break;
+                }
             };
-
-            return numbers.reduce(function(sum, current){
-                return sum + parseInt(current);
-            }, 0);
+            if (hasDelimeters){
+                customDelimeter = input[i];
+                reg = new RegExp(customDelimeter);
+                numbers = input.split(reg);
+            } else {
+                numbers[0] = input;
+            };
         }
 
+        return numbers.reduce(function(sum, current){
+            return sum + parseInt(current);
+        }, 0);
     };
 }
 
